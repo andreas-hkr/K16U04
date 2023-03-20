7f9d57de5ecce39b5a70283000bf4a188075d1f5
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-20 18:19:07 +0100

    Added starting value to comment

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Vi har tidigare skapat klassen Counter, nu är det dags att göra den säker
-        //   - Konstruktorn skall sätta intervallet
+        //   - Konstruktorn skall sätta intervallet + startvärdet
         //   - Getter och setter för värdet (glöm inte kontrollera att gitigt i settern)
 
     }
