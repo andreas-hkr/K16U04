4fbd30371c5a13d8069022683e6475413e0079c9
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-20 18:16:37 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        // Vi har tidigare skapat klassen Counter, nu är det dags att göra den säker
+        //   - Konstruktorn skall sätta intervallet
+        //   - Getter och setter för värdet (glöm inte kontrollera att gitigt i settern)
+
+    }
+}
+
+class Counter {
+    int value;
+    int min;
+    int max;
+
+    void init(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    void inc() {
+        if (value < max) {
+            value++;
+        } else {
+            throw new IllegalStateException("Ogiltigt värde");
+        }
+    }
+
+    void dec() {
+        if (value > min) {
+            value--;
+        } else {
+            throw new IllegalStateException("Ogiltigt värde");
+        }
+    }
+}
